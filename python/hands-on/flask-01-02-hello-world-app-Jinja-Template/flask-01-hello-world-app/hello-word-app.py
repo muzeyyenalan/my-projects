fc5834246758fb085d1eb57e4b67c4ad66ce87d6
muzeyyenalan <muzeyyenbas@gmail.com>
2022-01-18 23:32:56 +0300

    flask-01 ve flask-02 eklendi

---
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-word-app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World from Flask!!!"
+
+@app.route('/second')
+def second():
+    return 'Bize Her Yer Trabzon!!!!'
+
+@app.route('/third/subthird')
+def third():
+    return 'This is the subpage of third page'
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id number of this page is {id}'
+
+
+
+
+if __name__=='__main__':
+  app.run(debug=True)
+  
